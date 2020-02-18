2aea890ae389ade8cc511c041eddabc2d6ed19ac
Jideofor Nwoke <njusticej@gmail.com>
2020-02-18 18:51:31 +0100

    add user Model Class

---
+++ src/main/java/com/example/todo/model/User.java
@@ -0,0 +1,71 @@
+package com.example.todo.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(nullable = false)
+    @NotBlank
+    @Size(min = 4, max = 20, message = "Username should be of length: 4 - 20 characters")
+    private String userName;
+
+    @NotNull
+    @NotBlank
+    private  String password;
+
+    private boolean active = true;
+
+    private String roles = "user";
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
